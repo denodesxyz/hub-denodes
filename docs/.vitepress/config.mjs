014a0f3670941468625ec8dad3b9f9540eea66b9
Liubomyr Yarych <78175599+yarychok@users.noreply.github.com>
2023-12-29 10:10:55 +0200

    Added articles structure.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -6,10 +6,31 @@ export default defineConfig({
   description: "deNodes Hub description",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    logo: './assets/Logo.svg',
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Website', link: 'https://denodes.xyz/' },
+      { text: 'Twitter', link: 'https://twitter.com/_denodes' }
     ],
+
+    // sidebar: {
+    //   '/': [
+    //     {
+    //       text: 'Welcome to the deNodes Hub!',
+    //       children: [
+    //         { text: 'deNodes Team', link: '/denodes-team' },
+    //       ],
+    //     },
+    //     {
+    //       text: 'Namada',
+    //       children: [
+    //         { text: 'Project Overview', link: '/namada/' },
+    //         { text: 'Node Setup Guide', link: '/namada/node-setup-guide/' },
+    //         { text: 'Node Setup Guide (Russian)', link: '/namada/node-setup-guide-russian/' },
+    //         { text: 'FAQ', link: '/namada/faq/' },
+    //       ],
+    //     },
+    //   ],
+    // },
 
     sidebar: [
       {
